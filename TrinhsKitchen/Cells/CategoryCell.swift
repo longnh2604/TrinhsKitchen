436759f63ade66f98d2c52f1a847abd61ead3b99
longnh <longnh264@gmail.com>
2021-12-30 08:13:03 +0700

    [Task] Updated Cells

---
+++ TrinhsKitchen/Cells/CategoryCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCell.swift
+//  TrinhsKitchen
+//
+//  Created by longnh on 2021/12/29.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ TrinhsKitchen/Cells/ProductCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductCell.swift
+//  TrinhsKitchen
+//
+//  Created by longnh on 2021/12/29.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
